511378eb1f9da8e0b03735101a5daccb99c3518a
frankc60 <frankc60@gmail.com>
2019-02-02 19:25:56 +0100

    nothing

--- server/intents/dirIntent.js
+++ server/intents/dirIntent.js
@@ -14,7 +14,6 @@ module.exports.process = function process(intentData, cb) {
         cb(false,`stdout: ${data}`);
     });
 
-
     //error event handler for exec cmd
     ls.stderr.on('data', data => {
         cb(`stderr: ${data}`);
